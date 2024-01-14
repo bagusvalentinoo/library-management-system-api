2da0f3c32a20b7292fac56d0286a696a12aec5ba
Bagus <bagusvaalentino@gmail.com>
2024-01-14 09:31:07 +0700

    ✨ feat: Implement public API for book information.
    - Implemented Book Listing Endpoint.
    - Implemented Book Details Endpoint.

--- src/routes/api/v1/public.route.js
+++ src/routes/api/v1/public.route.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const ApiKey = require('@middlewares/api_key.middleware')
 const UserController = require('@controllers/public/user/user.controller')
+const BookController = require('@controllers/public/book/book.controller')
 
 // User Routes
 router.get('/users/check-username/:username', ApiKey, async (req, res) => {
@@ -11,6 +12,15 @@ router.get('/users/check-username/:username', ApiKey, async (req, res) => {
 
 router.get('/users/check-email/:email', ApiKey, async (req, res) => {
   await UserController.checkEmail(req, res)
+})
+
+// Book Routes
+router.get('/books', ApiKey, async (req, res) => {
+  await BookController.index(req, res)
+})
+
+router.get('/books/:id', ApiKey, async (req, res) => {
+  await BookController.show(req, res)
 })
 
 module.exports = router
